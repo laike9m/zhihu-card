0ee4f4654154f9e042c0b17bd9bf5ade0b7d52d1
laike9m <laike9m@gmail.com>
2016-06-22 16:07:00 +0800

    build

---
+++ jsdelivr/card.js
@@ -0,0 +1,161 @@
+function querystring() {
+  var href = window.location.href,
+    kv;
+  var params = href.slice(href.indexOf('?') + 1).split('&');
+  var qs = [];
+
+  for (i = 0; i < params.length; i++) {
+    kv = params[i].split('=');
+    qs.push(kv[0]);
+    qs[kv[0]] = kv[1];
+  }
+  return qs;
+}
+
+var qs = querystring();
+
+(function(d) {
+  var apiUrl = 'http://localhost:8000/v1/user/',
+    // var apiUrl = 'http://101.200.153.141:8000/v1/user/',
+    userUrl = "https://www.zhihu.com/people/",
+    i;
+  var eng2ch = {
+    "ask": "提问",
+    "answer": "回答",
+    "post": "文章",
+    "agree": "赞同",
+    "thanks": "感谢",
+    "follower": "关注者"
+  };
+  var eng2path = {
+    "ask": "asks",
+    "answer": "answers",
+    "post": "posts",
+    "agree": "",
+    "thanks": "",
+    "follower": "followers"
+  };
+
+  function store(key, value) {
+    try {
+      if (window.localStorage) {
+        if (value) {
+          value._timestamp = new Date().valueOf();
+          localStorage[key] = JSON.stringify(value);
+        } else {
+          var ret = localStorage[key];
+          if (ret) {
+            return JSON.parse(ret);
+          }
+          return null;
+        }
+      }
+    } catch (e) {}
+  }
+
+  function valueof() {}
+
+  function template(data) {
+    // 填充模版中的变量
+    var t = d.getElementById('zhihu-card');
+    var regex = /{([^}]+)}/g;
+    var text = t.innerHTML;
+    var m = text.match(regex);
+    for (i = 0; i < m.length; i++) {
+      // text = text.replace(m[i], valueof(data, m[i].slice(1, -1)));
+      text = text.replace(m[i], data[m[i].slice(1, -1)]);
+    }
+    return text;
+  }
+
+  function request(url, callback) {
+    var cache = store(url);
+    if (cache && cache._timestamp) {
+      console.log(new Date().valueOf());
+      // cache for 6 hours, set to 21600000 in production
+      if (new Date().valueOf() - cache._timestamp < 21600000) {
+        return callback(cache);
+      }
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+      callback(JSON.parse(xhr.response));
+    };
+    xhr.send();
+  }
+
+  function linky(card) {
+    // 填充 target, set iframe height
+    var links = card.getElementsByTagName('a');
+    for (i = 0; i < links.length; i++) {
+      (function(link) {
+        link.target = '_' + (qs.target || 'blank');
+      })(links[i]);
+    }
+    d.body.appendChild(card);
+    d.body.className = 'ready';
+    if (parent !== self && parent.postMessage) {
+      var height = Math.max(
+        d.body.scrollHeight,
+        d.documentElement.scrollHeight,
+        d.body.offsetHeight,
+        d.documentElement.offsetHeight,
+        d.body.clientHeight,
+        d.documentElement.clientHeight
+      );
+      parent.postMessage({
+        height: height,
+        sender: qs.identity || '*'
+      }, '*');
+    }
+  }
+
+  function zhihuCard(userhash) {
+
+    var url = apiUrl + userhash;
+    request(url, function(data) {
+
+      data = data || {};
+      var defaults = '?'; // 默认显示的字符
+      // message 存在说明data没有获取到数据，不存储，用 cache里的数据
+      if (typeof(data.name) == 'undefined') {
+        data = store(url) || data;
+      } else {
+        store(url, data);
+      }
+      var key1 = qs.key1,
+        key2 = qs.key2,
+        key3 = qs.key3;
+      data['key1_data'] = numberic(data[key1]) || defaults;
+      data['key2_data'] = numberic(data[key2]) || defaults;
+      data['key3_data'] = numberic(data[key3]) || defaults;
+      data['key1'] = eng2ch[key1];
+      data['key2'] = eng2ch[key2];
+      data['key3'] = eng2ch[key3];
+      data['key1_url'] = userUrl + data.domain + '/' + eng2path[key1];
+      data['key2_url'] = userUrl + data.domain + '/' + eng2path[key2];
+      data['key3_url'] = userUrl + data.domain + '/' + eng2path[key3];
+
+      var card = d.createElement('div');
+      card.className = 'zhihu-card';
+      card.innerHTML = template(data);
+      linky(card);
+    });
+  }
+
+  function numberic(num) {
+    if (num == null || typeof(num) == "undefined") return null;
+    if (num < 10000) {
+      return num.toString();
+    }
+    num = parseInt(num / 1000);
+    return num.toFixed(0) + 'k';
+  }
+
+  zhihuCard(qs.userhash);
+
+  function escape(text) {
+    return text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  }
+})(document);
---
+++ jsdelivr/widget.js
@@ -0,0 +1,71 @@
+(function(d) {
+  var base = "";
+  var i, count = 0;
+  var client_url, client_id, client_secret, client_theme;
+
+  function queryclass(name) {
+    if (d.querySelectorAll) {
+      return d.querySelectorAll('.' + name);
+    }
+    var elements = d.getElementsByTagName('div');
+    var ret = [];
+    for (i = 0; i < elements.length; i++) {
+      if (~elements[i].className.split(' ').indexOf(name)) {
+        ret.push(elements[i]);
+      }
+    }
+    return ret;
+  }
+
+  function querydata(element, name) {
+    return element.getAttribute('data-' + name);
+  }
+
+  function heighty(iframe) {
+    if (window.addEventListener) {
+      window.addEventListener('message', function(e) {
+        if (iframe.id === e.data.sender) {
+          iframe.height = e.data.height;
+        }
+      }, false);
+    }
+  }
+
+  function render(card) {
+    var theme = querydata(card, 'theme') || 'zhihu';
+    cardurl = base + 'theme/' + theme + '.html';
+    var userhash = querydata(card, 'userhash');
+    if (!userhash) {
+      return;
+    }
+    count += 1;
+    var width = querydata(card, 'width');
+    var height = querydata(card, 'height');
+    var key1 = querydata(card, 'key1');
+    var key2 = querydata(card, 'key2');
+    var key3 = querydata(card, 'key3');
+    var identity = 'ghcard-' + userhash + '-' + count;
+
+    var iframe = d.createElement('iframe');
+    iframe.setAttribute('id', identity);
+    iframe.setAttribute('frameborder', 0);
+    iframe.setAttribute('scrolling', 0);
+    iframe.setAttribute('allowtransparency', true);
+
+    var url = cardurl + '?userhash=' + userhash + '&identity=' + identity;
+    url += '&key1=' + key1 + '&key2=' + key2 + '&key3=' + key3;
+    iframe.src = url;
+    iframe.width = width || Math.min(card.parentNode.clientWidth || 400, 400);
+    if (height) {
+      iframe.height = height;
+    }
+    heighty(iframe);
+    card.parentNode.replaceChild(iframe, card);
+    return iframe;
+  }
+
+  var cards = queryclass('zhihu-card');
+  for (i = 0; i < cards.length; i++) {
+    render(cards[i]);
+  }
+})(document);
